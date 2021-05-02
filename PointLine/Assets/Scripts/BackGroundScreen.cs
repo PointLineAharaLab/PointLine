18f40be96c79df558e19f9a07bb8a335557a5eda
Kazushi Ahara <ahara@meiji.ac.jp>
2021-05-02 23:04:37 +0900

    pngをTextureへ変換する方法を調べた。

--- PointLine/Assets/Scripts/BackGroundScreen.cs
+++ PointLine/Assets/Scripts/BackGroundScreen.cs
@@ -32,4 +32,15 @@ public class BackGroundScreen : MonoBehaviour
             
 		}
     }
+
+    //PNGファイルを読み込んでTextureとして使う方法
+    // https://ugcj.com/png%E3%83%95%E3%82%A1%E3%82%A4%E3%83%AB%E3%82%92%E8%AA%AD%E3%81%BF%E8%BE%BC%E3%82%93%E3%81%A7texture%E3%81%A8%E3%81%97%E3%81%A6%E4%BD%BF%E3%81%86%E6%96%B9%E6%B3%95/
+
+    public Texture2D ReadPng(string path)
+    {
+        //byte[] bytes = File.ReadAllBytes(path);
+        Texture2D texture = new Texture2D(2, 2);
+        //texture.LoadImage(bytes);
+        return texture;
+    }
 }
